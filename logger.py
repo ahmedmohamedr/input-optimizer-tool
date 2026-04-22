4f06f7d43f9da8445381b3c192cc7f51541ffabe
0xreed <ahmedmohamedr62@gmail.com>
2026-04-22 04:15:13 -0500

    fix setup logging with rotation

--- logger.py
+++ logger.py
@@ -1,35 +1,19 @@
 import logging
+import os
+from logging.handlers import RotatingFileHandler
 
-class CustomLogger:
-    def __init__(self, name):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-        ch.setFormatter(formatter)
-        self.logger.addHandler(ch)
+def setup_logger(log_file='app.log', max_bytes=5*1024*1024, backup_count=3):
+    logger = logging.getLogger('input_optimizer_tool')
+    logger.setLevel(logging.DEBUG)
+    if not logger.hasHandlers():
+        handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    return logger
 
-    def debug(self, msg):
-        self.logger.debug(msg)
-
-    def info(self, msg):
-        self.logger.info(msg)
-
-    def warning(self, msg):
-        self.logger.warning(msg)
-
-    def error(self, msg):
-        self.logger.error(msg)
-
-    def critical(self, msg):
-        self.logger.critical(msg)
-
-# Example usage
 if __name__ == '__main__':
-    logger = CustomLogger('input_optimizer')
-    logger.info('Starting input optimization process...')
-    logger.debug('Debugging information...')
-    logger.warning('This is a warning message.')
-    logger.error('An error has occurred!')
-    logger.critical('Critical issue that needs immediate attention!')
+    log = setup_logger()
+    log.info('Logger is set up successfully.')
+    log.error('This is an error message.')
+    log.debug('Debugging information.')
