72c7a7b04f8e7a190e38c6a1706b3151e9793073
Ahmed Mohamed <ahmedmohamedr62@gmail.com>
2026-04-28 07:32:31 -0500

    implement input validation in loop

--- logger.py
+++ logger.py
@@ -1,19 +1,38 @@
 import logging
-from logging.handlers import RotatingFileHandler
 
-def setup_logger(log_file='app.log', max_bytes=5*1024*1024, backup_count=3):
-    logger = logging.getLogger('input_optimizer')
-    logger.setLevel(logging.DEBUG)
+class Logger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        console_handler.setFormatter(formatter)
+        self.logger.addHandler(console_handler)
 
-    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
+    def debug(self, message):
+        self.logger.debug(message)
 
-    if not logger.handlers:
-        logger.addHandler(handler)
-    return logger
+    def info(self, message):
+        self.logger.info(message)
 
-# Example usage:
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
+
 if __name__ == '__main__':
-    log = setup_logger()
-    log.info('Logger is set up and ready!')
+    log = Logger('GameInputValidator')
+    log.info('Logger has been initiated.')
+    
+    # Example input validation
+    inputs = ['valid_input', '', 'another_valid_input', None]
+    for i, user_input in enumerate(inputs):
+        if not isinstance(user_input, str) or not user_input:
+            log.warning(f'Input {i} is invalid: {user_input}')
+        else:
+            log.info(f'Input {i} is valid: {user_input}')
