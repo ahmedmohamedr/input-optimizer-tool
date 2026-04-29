fa5ad77e239521f837b5fe79713e0ddb6bf0fb99
Ahmed Mohamed <ahmedmohamedr62@gmail.com>
2026-04-29 04:01:39 -0500

    implement error handling for logger

--- logger.py
+++ logger.py
@@ -1,30 +1,47 @@
 import logging
 
-class GameLogger:
-    def __init__(self, name):
+class CustomFormatter(logging.Formatter):
+    def format(self, record):
+        log_fmt = "%(asctime)s - %(levelname)s - %(message)s"
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+class Logger:
+    def __init__(self, name, level=logging.INFO):
         self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
+        self.logger.setLevel(level)
         ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        ch.setFormatter(formatter)
+        ch.setLevel(level)
+        ch.setFormatter(CustomFormatter())
         self.logger.addHandler(ch)
 
-    def debug(self, msg):
-        self.logger.debug(msg)
+    def debug(self, message):
+        self.logger.debug(message)
 
-    def info(self, msg):
-        self.logger.info(msg)
+    def info(self, message):
+        self.logger.info(message)
 
-    def warning(self, msg):
-        self.logger.warning(msg)
+    def warning(self, message):
+        self.logger.warning(message)
 
-    def error(self, msg):
-        self.logger.error(msg)
+    def error(self, message):
+        self.logger.error(message)
 
-    def critical(self, msg):
-        self.logger.critical(msg)
+    def critical(self, message):
+        self.logger.critical(message)
 
-# Usage example
-# logger = GameLogger('my_game')
-# logger.info('Game started successfully.')
+    def exception(self, message):
+        self.logger.exception(message)
+
+if __name__ == '__main__':
+    logger = Logger('input_optimizer')
+    try:
+        logger.info('Starting the logger...')
+        # Simulate a warning
+        x = 5 / 0  # This will raise an exception
+    except ZeroDivisionError as e:
+        logger.error(f'Error occurred: {e}')
+    except Exception as e:
+        logger.exception(f'Unexpected error: {e}')
+    finally:
+        logger.info('Logging session ends.')
